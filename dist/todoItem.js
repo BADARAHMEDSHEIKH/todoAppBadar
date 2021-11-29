5f0599b7040ba309d021f3bc965114ea90c53818
BADAR-AHMED-SHEIKH <badr313merchant@gmail.com>
2021-11-30 01:56:10 +0500

    hello badar sheikh

---
+++ dist/todoItem.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TodoItem = void 0;
+console.log("helio");
+console.log("helio");
+class TodoItem {
+    constructor(id, task, complete = false) {
+        this.complete = false;
+        this.id = id;
+        this.task = task;
+        this.complete = complete;
+    }
+    printDetails() {
+        console.log(`${this.id}\t${this.task} ${this.complete
+            ? "\t(complete)" : ""}`);
+    }
+}
+exports.TodoItem = TodoItem;
